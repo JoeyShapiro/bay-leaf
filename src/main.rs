ba72b17dcbfbe0ab1130f16beb2c73116f027ab2
JoeyShapiro <joeyashapiro@gmail.com>
2022-10-28 08:49:05 -0500

    started rays

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use ncurses::*;
 
+const W_SIZE: i32 = 8;
+
 fn main() {
     println!("Hello, world!");
 
-    const W_SIZE: i32 = 8;
+    let mut objects: Vec<Vec<i32>> = Vec::new();
 
-    let mut world = [[[0u8; W_SIZE as usize]; W_SIZE as usize]; W_SIZE as usize];
     let mut x: i32 = 0;
     let mut y: i32 = 0;
     let mut z: i32 = 0;
@@ -13,7 +14,7 @@ fn main() {
     let mut phi: i32 = 0;
     let fov: i32 = 90;
     
-    world[5][5][5] = 2;
+    objects.push(vec![5, 5, 5]);
     
     initscr();
     
@@ -22,19 +23,17 @@ fn main() {
     getmaxyx(stdscr(), &mut max_y, &mut max_x);
 
     let mut perspective: Vec<Vec<i32>> = vec![vec![Default::default(); max_x as usize]; max_y as usize];
-    perspective[5][5] = 1;
     
     loop {
         let input = getch();
         
         clear();
-        world[x as usize][y as usize][z as usize] = 0;
         
         if input == 119 && x < W_SIZE-1 { // 
             x+=1;
         } else if input == 115 && x > 0 {
             x-=1;
-        } else if input == 97 && y > 0{ // 
+        } else if input == 97 && y > 0 { // 
             y-=1;
         } else if input == 100 && y < W_SIZE-1 {
             y+=1;
@@ -56,10 +55,12 @@ fn main() {
             phi = phi.rem_euclid(360);
         }
 
-        world[x as usize][y as usize][z as usize] = 1;
-
+        let ray_step: f64 = fov as f64 / max_x as f64;
+        let mut ray_angle: f64 = (theta as f64 - fov as f64 / 2.0).rem_euclid(360.0);
         for screen_y in 0..max_y {
             for screen_x in 0..max_x {
+                ray_angle+=ray_step;
+                perspective[screen_y as usize][screen_x as usize] = send_ray(x, y, z, ray_angle, &objects);
                 if perspective[screen_y as usize][screen_x as usize] == 1 {
                     mvprintw(screen_y, screen_x, &perspective[screen_y as usize][screen_x as usize].to_string());
                 }
@@ -81,4 +82,45 @@ fn main() {
 
         refresh();
     }
+}
+
+fn send_ray(x: i32, y: i32, z: i32, theta: f64, objects: &std::vec::Vec<std::vec::Vec<i32>>) -> i32 {
+    let mut x_vec = theta.cos();
+    let mut y_vec = theta.sin();
+
+    let mut x_prime = x as f64 + x_vec;
+    let mut y_prime = y as f64 + y_vec;
+
+    while x_prime <= W_SIZE as f64 && x_prime >= 0.0 && y_prime <= W_SIZE as f64 && y_prime >= 0.0 {
+        // mvprintw(0, 0, &("Loading: ".to_owned()+&x_prime.to_string()));
+        // refresh();
+
+        x_vec = theta.cos();
+        y_vec = theta.sin();
+    
+        x_prime += x_vec;
+        y_prime += y_vec;
+    
+        for object in objects {
+            if is_passing_through(x_prime, y_prime, &object) {
+                return 1;
+            }
+        }
+    }
+
+    return 0;
+}
+
+fn is_passing_through(x: f64, y: f64, object: &std::vec::Vec<i32>) -> bool {
+    if y >= object[0] as f64 && y <= object[0] as f64 + 1.0 {
+        if x >= object[1] as f64 && x <= object[1] as f64 + 1.0 {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn is_touching_or_worse(a: std::vec::Vec<i32>, b: std::vec::Vec<i32>) -> bool {
+    return false;
 }
